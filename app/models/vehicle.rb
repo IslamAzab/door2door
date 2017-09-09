145337c05dfba621a6728feb3ccff0d930ebae80
Islam Azab <islam.3azab@gmail.com>
2017-09-09 15:40:50 +0200

    added validations for vehicle attributes

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,4 +1,19 @@
 class Vehicle < ApplicationRecord
   include UuidHelper
   enum vehicle_type: [ :bus, :taxi, :tram, :train ]
+
+  validates :uuid, format: { with: /[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}/,
+    message: "%{value} should be in UUID format." }
+  validates :latitude, inclusion: { in: -90..90,
+    message: "%{value} should be between -90 and 90." }
+  validates :longitude, inclusion: { in: -180..180,
+    message: "%{value} should be between -180 and 180." }
+  validates :heading, inclusion: { in: 0..359,
+    message: "%{value} should be between 0 and 359." }
+
+  validate :timestamp_is_valid_datetime
+
+  def timestamp_is_valid_datetime
+    errors.add(:timestamp, 'must be a valid datetime') if ((DateTime.parse(happened_at) rescue ArgumentError) == ArgumentError)
+  end
 end
