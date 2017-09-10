f3c5f48aebbdf7ce8d9e2c55e0302cbde65df24c
Islam Azab <islam.3azab@gmail.com>
2017-09-11 00:43:53 +0200

    change uuid helper to generate uuid but not with upcase characters

--- lib/uuid_helper.rb
+++ lib/uuid_helper.rb
@@ -1,11 +1,10 @@
 module UuidHelper
-  # require "uuidtools"
   def self.included(base)
     base.before_create :assign_uuid
   end
 
   private
   def assign_uuid
-    self.uuid = UUIDTools::UUID.timestamp_create().to_s.upcase if uuid.blank?
+    self.uuid = UUIDTools::UUID.timestamp_create().to_s if uuid.blank?
   end
 end
